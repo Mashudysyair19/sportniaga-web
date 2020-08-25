ccf547741ae9673de314b42f288e09eff778e28d
Dzyfhuba <hafidz21ub@gmail.com>
2020-08-25 18:13:26 +0700

    menambahkan role | mantap | perlu testing

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,9 +3,9 @@
 
 <div id="previewCarousel" class="carousel slide bg-dark" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#previewCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#previewCarousel" data-slide-to="1"></li>
-    <li data-target="#previewCarousel" data-slide-to="2"></li>
+    <li style="width: 10px; height: 10px; border-radius: 100%" data-target="#previewCarousel" data-slide-to="0" class="active"></li>
+    <li style="width: 10px; height: 10px; border-radius: 100%" data-target="#previewCarousel" data-slide-to="1"></li>
+    <li style="width: 10px; height: 10px; border-radius: 100%" data-target="#previewCarousel" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
--- routes/web.php
+++ routes/web.php
@@ -13,44 +13,63 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return View::make('pages.home');
-});
 
-Route::get('/jerseybola', function () {
-    return View::make('pages.jerseybola');
-});
 
-Route::get('/jerseyfutsal', function () {
-    return View::make('pages.jerseyfutsal');
-});
 
-Route::get('/sepatubola', function () {
-    return View::make('pages.sepatubola');
-});
-
-Route::get('/sepatufutsal', function () {
-    return View::make('pages.sepatufutsal');
-});
-
-Route::get('/adm00n', function () {
-	return View::make('pages.adm00n.mainpage');
-});
-
-Route::get('/adm00n', 'Adm00nUploadController@upload');
-
-Route::post('/adm00n/process', 'Adm00nUploadController@upload_process');
-
-Route::get('/adm00n/delete/{id}', 'Adm00nUploadController@delete');
 
 Auth::routes();
-
 Route::get('/', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/default', function () {
 	return View::make('pages.adm00n.mainpage');
+});
+
+Route::group(['middleware' => ['auth', 'roleCheck:admin']], function(){
+	Route::get('/adm00n', 'Adm00nUploadController@upload');
+	Route::post('/adm00n/process', 'Adm00nUploadController@upload_process');
+	Route::get('/adm00n/delete/{id}', 'Adm00nUploadController@delete');
+});
+
+Route::group(['middleware' => ['auth', 'roleCheck:user, admin']], function(){
+	Route::get('/', function () {
+	    return View::make('pages.home');
+	});
+
+	Route::get('/jerseybola', function () {
+	    return View::make('pages.jerseybola');
+	});
+
+	Route::get('/jerseyfutsal', function () {
+	    return View::make('pages.jerseyfutsal');
+	});
+
+	Route::get('/sepatubola', function () {
+	    return View::make('pages.sepatubola');
+	});
+
+	Route::get('/sepatufutsal', function () {
+	    return View::make('pages.sepatufutsal');
+	});
+});
+
+Route::group(['middleware' => 'guest'], function(){
+	Route::get('/', function () {
+	    return View::make('pages.home');
+	})->name('home');
+
+	Route::get('/jerseybola', function () {
+	    return View::make('pages.jerseybola');
+	});
+
+	Route::get('/jerseyfutsal', function () {
+	    return View::make('pages.jerseyfutsal');
+	});
+
+	Route::get('/sepatubola', function () {
+	    return View::make('pages.sepatubola');
+	});
+
+	Route::get('/sepatufutsal', function () {
+	    return View::make('pages.sepatufutsal');
+	});
 });
